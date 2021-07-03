03d04cd402c60a1f40c5fd5fa00052b244c57b7c
helionporto <helionporto@hotmail.com>
2021-07-02 22:55:05 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './assets/styles/custom.scss';
+import './App.css';
+
+const  App = () =>{
+  return (
+    <div className="App">
+      <h1>Top carros</h1>
+    </div>
+  );
+}
+
+export default App;
