9926e3df0fc8571432d3a9ee0eac7c570a8de91c
helionporto <helionporto@hotmail.com>
2021-07-04 22:11:23 -0400

    Navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from './components/Navbar';
 
-const  App = () =>{
+const App = () => {
   return (
-    <div className="App">
+    <>
+      <Navbar/>
       <h1>Top carros</h1>
-    </div>
+    </>
   );
 }
-
 export default App;
